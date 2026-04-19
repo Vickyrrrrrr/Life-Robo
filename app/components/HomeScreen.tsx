52a659423100af2ea31efecfcfea8aab70ae1c45
VxkY <89289497+Vickyrrrrrr@users.noreply.github.com>
2026-04-19 16:32:30 +0530

    redesign: clean HomeScreen wrapper with main landmark

--- app/components/HomeScreen.tsx
+++ app/components/HomeScreen.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { AuroraHero } from "./AuroraHero";
 
-const HomeScreen = () => (
-  <main id="main-content" className="site-main">
-    <AuroraHero />
-  </main>
-);
+const HomeScreen = () => {
+  return (
+    <main id="main-content" className="site-main">
+      <AuroraHero />
+    </main>
+  );
+};
 
 export default HomeScreen;
